6d3329aeac7ec8bf3365ee60380ed49e25910495
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-09-24 21:20:56 +0200

    Beginning

--- SimpleInventory/Program.cs
+++ SimpleInventory/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class InventoryManager
+{
+    static List<Item> inventory = new List<Item>();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to SimpleInventory!");
+
+    }
+}
+
+
+class Item
+{
+    public string? Name { get; set; }
+    public int? Quantity { get; set; }
+    public decimal? Price { get; set; }
+}
