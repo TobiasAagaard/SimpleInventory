760601d915bb4f93da0c3322c1ee5acffe9efcd9
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-11-04 11:57:54 +0100

    Starting to modularize the code

--- SimpleInventory/Program.cs
+++ SimpleInventory/Program.cs
@@ -433,11 +433,5 @@ class Program
         }
     }
     }
-    class Item
-    {
-        public string? Id { get; set; }
-        public string? Name { get; set; }
-        public int? Quantity { get; set; }
-        public decimal? Price { get; set; }
-    }
+
 
