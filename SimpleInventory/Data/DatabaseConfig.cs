44cef596a4a3a5e0b444eed000fd07deae2a8163
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-11-04 12:03:59 +0100

    Data sets

---
+++ SimpleInventory/Data/DatabaseConfig.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SimpleInventory.Data
+{
+    public static class DatabaseConfig
+    {
+        public static string? ConnectionString { get; private set; }
+
+        public static void LoadConnectionString()
+        {
+            var jsonText = File.ReadAllText("appsettings.json");
+            var jsonObj = JObject.Parse(jsonText);
+
+            var server = jsonObj["ConnectionStrings"]["MySqlConnection"]["Server"];
+            var database = jsonObj["ConnectionStrings"]["MySqlConnection"]["Database"];
+            var userId = jsonObj["ConnectionStrings"]["MySqlConnection"]["UserId"];
+            var password = jsonObj["ConnectionStrings"]["MySqlConnection"]["Password"];
+
+            ConnectionString = $"Server={server};Database={database};User ID={userId};Password={password};";
+        }
+    }
+}
---
+++ SimpleInventory/Data/ItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using SimpleInventory.Models;
+
+namespace SimpleInventory.Data
+{
+    public class ItemRepository
+    {
+        public void AddItem(Item item) { /* Logic for adding an item */ }
+        public void DeleteItem(string id) { /* Logic for deleting an item */ }
+        public void UpdateItem(Item item) { /* Logic for updating an item */ }
+        public List<Item> ViewInventory() { /* Logic for viewing all items */ }
+        public Item? GetItemByIdOrName(string searchTerm) { /* Logic for finding an item */ }
+        public bool CheckIfIdExists(string id) { /* Logic for checking item existence */ }
+    }
+}
