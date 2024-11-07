346a508f51a2a5ef4f739548b0191201b237d3a0
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-11-07 09:56:02 +0100

    InventoryOperation
    I will remake the functions and make it atomic design and this will be teh executer

---
+++ SimpleInventory/Operations/InventoryOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventory.Operations
+{
+    public interface InventoryOperation
+    {
+        void Execute();
+    }
+}
